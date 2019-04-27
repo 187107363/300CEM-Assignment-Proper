d966b54b48dc2c234767dcee701509fa9ff1571c
187107363 <43198174+187107363@users.noreply.github.com>
2019-04-27 17:51:29 +0800

    create Drop point

---
+++ app/src/main/java/com/example/myapplication/createDropPoint.java
@@ -0,0 +1,107 @@
+package com.example.myapplication;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.Response;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class createDropPoint extends Fragment {
+    TextView info;
+    RequestQueue requestQueue;
+
+    String baseUrl = "https://backend300cem.herokuapp.com/type/all";
+    public createDropPoint() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_create_drop_point, container, false);
+        requestQueue = Volley.newRequestQueue(this.getContext());
+        this.info = (TextView) v.findViewById(R.id.textView_CDP);
+        getRepoList();
+        return v;
+    }
+    private void clearRepoList() {
+        this.info.setText("");
+    }
+
+    private void addToRepoList(String type) {
+        String strRow = type;
+        String currentText = info.getText().toString();
+        this.info.setText(currentText + "\n\n" + strRow);
+    }
+
+    private void setRepoListText(String str) {
+        this.info.setText(str);
+    }
+
+    private void getRepoList() {
+        // Next, we create a new JsonArrayRequest. This will use Volley to make a HTTP request
+        // that expects a JSON Array Response.
+        // To fully understand this, I'd recommend reading the office docs: https://developer.android.com/training/volley/index.html
+        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, this.baseUrl,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Check the length of our response (to see if the user has any repos)
+                        if (response.length() > 0) {
+                            // The user does have repos, so let's loop through them all.
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    // For each repo, add a new line to our repo list.
+                                    JSONObject jsonObj = response.getJSONObject(i);
+                                    String type = jsonObj.get("type").toString();
+                                    addToRepoList(type);
+                                } catch (JSONException e) {
+                                    // If there is an error then output this to the logs.
+                                    Log.e("Volley", "Invalid JSON Object.");
+                                }
+
+                            }
+                        } else {
+                            // The user didn't have any repos.
+                            setRepoListText("No repos found.");
+                        }
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // If there a HTTP error then add a note to our repo list.
+                        setRepoListText("Error while calling REST API");
+                        Log.e("Volley", error.toString());
+                    }
+                }
+        );
+        // Add the request we just defined to our request queue.
+        // The request queue will automatically handle the request as soon as it can.
+        requestQueue.add(arrReq);
+    }
+
+
+}
